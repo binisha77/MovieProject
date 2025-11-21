07da6d97c45c331cef676a23016dd848b3effc4e
sovitha <khadkabinisha48@gmail.com>
2025-11-21 13:05:25 +0545

    api added

---
+++ client/src/services/api.js
@@ -0,0 +1,13 @@
+const API_key ="199cc4a6a6e034d947209a3f7788f819"
+const BASE_URL = " https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () =>{
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_key}`)
+  const data = await response.json()
+  return data.results
+}
+export const searchMovies = async (query) =>{
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_key}&query=${encodeURIComponent(query)}`)
+  const data = await response.json()
+  return data.results
+}
